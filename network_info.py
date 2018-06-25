c990186b2d934793d602dc64b11eefeeda2d0c86
git_dave <ctchapman@gmail.com>
2018-06-25 15:34:24 -0700

    added git ignore for idea files, added in test program for getting ip address

--- network_info.py
+++ network_info.py
@@ -2,3 +2,16 @@
 #run traceroute to provided address and back
 #display results
 #offer to send reults to a printer also
+
+import socket
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
