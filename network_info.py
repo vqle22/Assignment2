ab7d891f3d27b3f21ca75a9c401f34a5a12a5d95
vqle22 <vinhl@uw.edu>
2018-06-25 15:48:26 -0700

    updated function to get the local host info
    return the IPv4, IPv6, and Subnet Mask of local host.

--- network_info.py
+++ network_info.py
@@ -3,15 +3,18 @@
 #display results
 #offer to send reults to a printer also
 
-import socket
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
+import netifaces
+
+def localhost_info():
+    for i in netifaces.interfaces():
+        try:
+            # Address
+            print("IPv4: ", netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr'])
+            print("Subnet Mask: ", netifaces.ifaddresses(i)[netifaces.AF_INET][0]['netmask'])
+            print("IPv6: ", netifaces.ifaddresses(i)[netifaces.AF_INET6][0]['addr'])
+            print("Subnet Mask: ", netifaces.ifaddresses(i)[netifaces.AF_INET6][0]['netmask'])          
+            print("\n")
+        except:pass
+# This is the IP address of the localhost
+
+localhost_info()
