67084b2b79d0a2faa0aeb2bd708b26d5029db8e9
vqle22 <vinhl@uw.edu>
2018-06-27 20:00:54 -0700

    Update network_info.py
    the program will now prompt user to choose between FQDN or IPA.

--- network_info.py
+++ network_info.py
@@ -6,7 +6,8 @@
 import sys
 import ipaddress
 import socket
-import  pexpect
+import pexpect
+import netifaces
 
 
 ################## use Vinh's code here, get the info about this machine ##########################
@@ -23,27 +24,34 @@ def localhost_info():
 # This is the IP address of the localhost
 localhost_info()
 
-################## now get the target from the user ###############################################
-user_input = input("Enter a Fully Qualified Domain Name or IPv4 or IPv6 address\n")
-print ("your input was: ", user_input)
-
-###############choose between it being an address or an fqdn
+################## now get the target from the user #################################
+################## choose between it being an address or an fqdn ###############
+user_input = ""
+while user_input != "FQDN" and user_input != "IPA":
+    user_input = input("Enter FQDN or IPA: ")
+    if user_input != "FQDN" and user_input != "IPA":
+        print("You must type FQDN or IPA!")
 
 #########if it is an ip address, then do this
-
-#######try to create an ip address from the input, catch faulty values
-try:
-    input_ip_address = ipaddress.ip_network(user_input)
-    print("You entered an IPAddress: ", input_ip_address)
-    #do we need to be able to tell what kind of address?
-    print("ip address version: ", input_ip_address.version)
-except ValueError:
-    print("input was not a correct IPv4 or IPv6 address")
-else:
-    target_address = input_ip_address
-    print("target address: ", target_address)
+#######try to create an ip address from the input, catch faulty values #######
+if user_input == "IPA":
+    ipa_input = input("Enter a Fully Qualified Domain Name or IPv4 or IPv6 address: \n")
+    print ("your input was: ", ipa_input)
+    try:
+        input_ip_address = ipaddress.ip_network(ipa_input)
+        print("You entered an IPAddress: ", input_ip_address)
+        #do we need to be able to tell what kind of address?
+        print("ip address version: ", input_ip_address.version)
+    except ValueError:
+        print("input was not a correct IPv4 or IPv6 address")
+    else:
+        target_address = input_ip_address
+        print("target address: ", target_address)
 
 #if it is not an ip address, then do this (includes fqdn name errors)
+
+# if user_input = "FQDN":
+
 #we need to look at the dns to get info about the fqdn we got as user input
 #print("\n", socket.getaddrinfo(user_input, 80, family=0, type=0, proto=0, flags=0))
 #socket.getaddrinfo(user_input, 80, family=0, type=0, proto=0, flags=0))
@@ -53,7 +61,7 @@ else:
 #run the traceroute
 
 #run the ping from here to there using bash commands!
-bash_command_string = 'ping -c 10 ' + user_input
+bash_command_string = 'ping -c 10 ' + ipa_input
 #child = pexpect.spawn('ping -c 5 www.google.com')
 child = pexpect.spawn(bash_command_string)
 
@@ -65,7 +73,3 @@ while 1:
 
 
 ################## traceroute ran, output displayed, not prompt to send to a printer ##############
-
-
-
-
